93545027094936ba70eddbda322cbb2408583f2e
Jorge Trad <jetradl@gmail.com>
2021-10-05 18:57:18 -0500

    State Management

--- to-do-machine/src/TodoSearch.jsx
+++ to-do-machine/src/TodoSearch.jsx
@@ -2,16 +2,25 @@ import React from 'react';
 import './TodoSearch.css';
 
 function TodoSearch() {
+  const [ searchValue, setSearchValue ] = React.useState('');
+
+
   const onSearchValueChange = (event) => {
     console.log(event.target.value);
+    setSearchValue(event.target.value)
   };
   
   return (
-    <input
-      className="TodoSearch"
-      placeholder="Cebolla"
-      onChange={onSearchValueChange}
-    />
+    <React.Fragment>
+      <input
+        className="TodoSearch"
+        placeholder="Cebolla"
+        /* onChange={onSearchValueChange} */
+        value={ searchValue }
+        onChange={ onSearchValueChange }
+      />
+      <p>{ searchValue }</p>
+    </React.Fragment>
   );
 }
 
